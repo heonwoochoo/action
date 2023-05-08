b66ac01a4ac2a9771e97eeea738c6f5c869fa665
heonwoochoo <cngjsdn9@gmail.com>
2023-05-09 00:37:29 +0900

    Create dialogue box widget

---
+++ portfolio/Source/portfolio/Private/HUD/NPC/DialogueBox.cpp
@@ -0,0 +1,137 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "HUD/NPC/DialogueBox.h"
+#include "Components/Button.h"
+#include "Components/Image.h"
+#include "Components/TextBlock.h"
+#include "Kismet/GameplayStatics.h"
+#include "GameMode/DefaultGameMode.h"
+
+void UDialogueBox::NativeConstruct()
+{
+	InitCloseButton();
+	InitPrevButton();
+	InitNextButton();
+}
+
+void UDialogueBox::OnHoveredCloseButton()
+{
+	if (CloseButtonImage && ActivatedButtonImage)
+	{
+		CloseButtonImage->SetBrushFromTexture(ActivatedButtonImage);
+	}
+}
+
+void UDialogueBox::OnUnhoveredCloseButton()
+{
+	if (CloseButtonImage && DeactivatedButtonImage)
+	{
+		CloseButtonImage->SetBrushFromTexture(DeactivatedButtonImage);
+	}
+}
+
+void UDialogueBox::OnClickedCloseButton()
+{
+
+	// 효과음
+	ADefaultGameMode* DefaultGameMode = Cast<ADefaultGameMode>(UGameplayStatics::GetGameMode(this));
+	if (DefaultGameMode)
+	{
+		DefaultGameMode->PlayChangeButtonClickSound();
+	}
+}
+
+void UDialogueBox::OnHoveredPrevButton()
+{
+	if (PrevButtonImage && ActivatedButtonImage)
+	{
+		PrevButtonImage->SetBrushFromTexture(ActivatedButtonImage);
+	}
+}
+
+void UDialogueBox::OnUnhoveredPrevButton()
+{
+	if (PrevButtonImage && DeactivatedButtonImage)
+	{
+		PrevButtonImage->SetBrushFromTexture(DeactivatedButtonImage);
+	}
+}
+
+void UDialogueBox::OnClickedPrevButton()
+{
+	// 효과음
+	ADefaultGameMode* DefaultGameMode = Cast<ADefaultGameMode>(UGameplayStatics::GetGameMode(this));
+	if (DefaultGameMode)
+	{
+		DefaultGameMode->PlayChangeButtonClickSound();
+	}
+}
+
+void UDialogueBox::OnHoveredNextButton()
+{
+	if (NextButtonImage && ActivatedButtonImage)
+	{
+		NextButtonImage->SetBrushFromTexture(ActivatedButtonImage);
+	}
+}
+
+void UDialogueBox::OnUnhoveredNextButton()
+{
+	if (NextButtonImage && DeactivatedButtonImage)
+	{
+		NextButtonImage->SetBrushFromTexture(DeactivatedButtonImage);
+	}
+}
+
+void UDialogueBox::OnClickedNextButton()
+{
+	// 효과음
+	ADefaultGameMode* DefaultGameMode = Cast<ADefaultGameMode>(UGameplayStatics::GetGameMode(this));
+	if (DefaultGameMode)
+	{
+		DefaultGameMode->PlayChangeButtonClickSound();
+	}
+}
+
+void UDialogueBox::InitCloseButton()
+{
+	if (CloseButton)
+	{
+		CloseButton->OnHovered.AddDynamic(this, &UDialogueBox::OnHoveredCloseButton);
+		CloseButton->OnUnhovered.AddDynamic(this, &UDialogueBox::OnUnhoveredCloseButton);
+		CloseButton->OnClicked.AddDynamic(this, &UDialogueBox::OnClickedCloseButton);
+	}
+	if (CloseButtonImage && DeactivatedButtonImage)
+	{
+		CloseButtonImage->SetBrushFromTexture(DeactivatedButtonImage);
+	}
+}
+
+void UDialogueBox::InitPrevButton()
+{
+	if (PrevButton)
+	{
+		PrevButton->OnHovered.AddDynamic(this, &UDialogueBox::OnHoveredPrevButton);
+		PrevButton->OnUnhovered.AddDynamic(this, &UDialogueBox::OnUnhoveredPrevButton);
+		PrevButton->OnClicked.AddDynamic(this, &UDialogueBox::OnClickedPrevButton);
+	}
+	if (PrevButtonImage && DeactivatedButtonImage)
+	{
+		PrevButtonImage->SetBrushFromTexture(DeactivatedButtonImage);
+	}
+}
+
+void UDialogueBox::InitNextButton()
+{
+	if (NextButton)
+	{
+		NextButton->OnHovered.AddDynamic(this, &UDialogueBox::OnHoveredNextButton);
+		NextButton->OnUnhovered.AddDynamic(this, &UDialogueBox::OnUnhoveredNextButton);
+		NextButton->OnClicked.AddDynamic(this, &UDialogueBox::OnClickedNextButton);
+	}
+	if (NextButtonImage && DeactivatedButtonImage)
+	{
+		NextButtonImage->SetBrushFromTexture(DeactivatedButtonImage);
+	}
+}
---
+++ portfolio/Source/portfolio/Public/HUD/NPC/DialogueBox.h
@@ -0,0 +1,96 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "DialogueBox.generated.h"
+
+class UButton;
+class UImage;
+class UTextBlock;
+class UTexture2D;
+
+UCLASS()
+class PORTFOLIO_API UDialogueBox : public UUserWidget
+{
+	GENERATED_BODY()
+	
+protected:
+	virtual void NativeConstruct() override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (BindWidget))
+	UTextBlock* DialogueText;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (BindWidget))
+	UImage* NPCImage;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (BindWidget))
+	UImage* CloseButtonImage;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (BindWidget))
+	UButton* CloseButton;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (BindWidget))
+	UTextBlock* CloseText;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (BindWidget))
+	UImage* PrevButtonImage;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (BindWidget))
+	UButton* PrevButton;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (BindWidget))
+	UTextBlock* PrevText;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (BindWidget))
+	UImage* NextButtonImage;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (BindWidget))
+	UButton* NextButton;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (BindWidget))
+	UTextBlock* NextText;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Textrue")
+	UTexture2D* ActivatedButtonImage;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Textrue")
+	UTexture2D* DeactivatedButtonImage;
+
+	UFUNCTION()
+	void OnHoveredCloseButton();
+
+	UFUNCTION()
+	void OnUnhoveredCloseButton();
+
+	UFUNCTION()
+	void OnClickedCloseButton();
+
+	UFUNCTION()
+	void OnHoveredPrevButton();
+
+	UFUNCTION()
+	void OnUnhoveredPrevButton();
+
+	UFUNCTION()
+	void OnClickedPrevButton();
+
+	UFUNCTION()
+	void OnHoveredNextButton();
+
+	UFUNCTION()
+	void OnUnhoveredNextButton();
+
+	UFUNCTION()
+	void OnClickedNextButton();
+
+private:
+	void InitCloseButton();
+	void InitPrevButton();
+	void InitNextButton();
+
+public:
+	
+
+};
